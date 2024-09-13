4dcdbc550bc5f51ed271bece8b7ee141c3d6d9e0
Robson Oliveira <robsonempdigital@gmail.com>
2024-09-13 00:20:42 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*O ATIVIDADE EM SALA 
+Para praticar 
+No mesmo arquivo que criamos, siga as seguintes instruções: 
+
+- Utilizando o prompt, receba 2 números do usuário.
+- Realize a soma desses 2 números e exiba o resultado no console.
+- Realize o produto desses 2 números e exiba o resultado no alert */
+
+var num1 = parseFloat(prompt("Digite aqui o item 1:"));
+var num2 = parseFloat(prompt("Digite aqui o item 2:"));
+
+var soma = num1 + num2
+console.log("A soma dos números é " + soma)
+var produto = num1 * num2;
+alert("O produto dos número é " + produto);
+
+
+
